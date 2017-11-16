271193bc3ca52c538f6220d91d301b688afdbbc9
knoten85 <philipp.schweiger85@googlemail.com>
2017-11-16 10:27:24 +0100

    init

---
+++ app/src/main/java/halloandroid/sabel/com/halloandroid/MainActivity.java
@@ -0,0 +1,77 @@
+package halloandroid.sabel.com.halloandroid;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.KeyEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends Activity {
+
+    private TextView tvNnachricht;
+    private Button btnWeiterFertig;
+    private EditText etEingabe;
+    private boolean ersterKlick;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        tvNnachricht = findViewById(R.id.tv_nachricht);
+        btnWeiterFertig = findViewById(R.id.btn_weiter_fertig);
+        etEingabe = findViewById(R.id.et_eingabe);
+
+        tvNnachricht.setText(R.string.willkommen);
+        btnWeiterFertig.setText(R.string.weiter);
+        btnWeiterFertig.setEnabled(false);
+
+        ersterKlick = true;
+
+        btnWeiterFertig.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(ersterKlick) {
+                    tvNnachricht.setText(getString(R.string.hallo, etEingabe.getText()));
+                    etEingabe.setVisibility(View.INVISIBLE);
+                    btnWeiterFertig.setText(R.string.fertig);
+                    ersterKlick = false;
+                } else {
+                    finish();
+                }
+            }
+        });
+
+        etEingabe.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView textView, int i, KeyEvent keyEvent) {
+                if (btnWeiterFertig.isEnabled()){
+                    btnWeiterFertig.performClick();
+                }
+                return true;
+            }
+        });
+
+        etEingabe.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                btnWeiterFertig.setEnabled(editable.toString().contains(" "));
+            }
+        });
+
+    }
+}
